af6a5fbd5a7556a684c3044de27da741e1165344
qdtmar9-glitch <qdtmar9@gmail.com>
2026-07-22 18:53:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+    if (navLinks.classList.contains("active")) {
+        menuToggle.textContent = "✕";
+    } else {
+        menuToggle.textContent = "☰";
+    }
+});
