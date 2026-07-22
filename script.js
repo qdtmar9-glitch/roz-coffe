07314f101c4dada2559b5a36d2c9c5daad65db0e
qdtmar9-glitch <qdtmar9@gmail.com>
2026-07-22 19:24:51 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,52 @@ menuToggle.addEventListener("click", () => {
         menuToggle.textContent = "☰";
     }
 });
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+        topBtn.style.display = "block";
+    }else{
+        topBtn.style.display = "none";
+    }
+
+});
+
+topBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+});
+const hiddenElements = document.querySelectorAll(".popular-card,.card,.box");
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+
+    });
+
+});
+
+hiddenElements.forEach(el=>{
+    el.classList.add("hidden");
+    observer.observe(el);
+});
+window.addEventListener("load",()=>{
+
+    const loader=document.getElementById("loader");
+
+    setTimeout(()=>{
+
+        loader.classList.add("hide");
+
+    },1200);
+
+});
